f41f3514a2f96a4dfdac23250bd8faaafbf1be54
İrfan DUMAN <69718844+irfandumanx@users.noreply.github.com>
2025-03-17 16:46:24 +0300

    Truncate action string to the maximum allowed length of 85 characters.
    Truncate action string to the maximum allowed length of 85 characters.

--- src/Tags/Recaptcha.php
+++ src/Tags/Recaptcha.php
@@ -53,7 +53,7 @@ class Recaptcha extends Tags
     protected function v3()
     {
         $siteKey = config('recaptcha.recaptcha_v3.site_key');
-        $action = e(str_replace('-', '_', request()->path()));
+        $action = e(substr(str_replace('-', '_', request()->path()), 0, 85));
         $verifyOnPageLoad = config('recaptcha.recaptcha_v3.verify_on_page_load', true) ? 'true' : 'false';
 
         return <<<SCRIPT
