e56cad82999fca3dce4caeebaaad902f1892d6d4
Martin Dub <49438284+martinoak@users.noreply.github.com>
2026-06-30 10:47:05 +0200

    Remove redundant curl_close in RecaptchaV3.php
    Removed unnecessary curl_close call after executing curl.  `curl_close` and `curl_share_close` functions are deprecated because they are no-op in PHP >= 8.0

--- src/Services/RecaptchaV3.php
+++ src/Services/RecaptchaV3.php
@@ -25,7 +25,6 @@ class RecaptchaV3
         curl_setopt($ch, CURLOPT_POST, 0);
         curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
         $output = curl_exec($ch);
-        curl_close($ch);
 
         $result = json_decode($output);
 
